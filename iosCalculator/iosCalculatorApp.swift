a49399cb4d69187e9431f8b06b9c2ab18637a219
VaibhavRajani <vaibhav.rajanivit@gmail.com>
2023-10-06 13:35:32 -0400

    Initial Commit

---
+++ iosCalculator/iosCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosCalculatorApp.swift
+//  iosCalculator
+//
+//  Created by Vaibhav Rajani on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct iosCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
